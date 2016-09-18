2b694c6a0b66f8b98d87e9f268f3b4cd6f046a47
joanna-chen <joannachen.x@gmail.com>
2016-09-17 21:32:24 -0400

    uhhh

--- js/process.js
+++ js/process.js
@@ -1,18 +1,10 @@
 var currTrackedStocks = new Array();
 var rateOfReturn = 0;
+var JSONresponse = {};
+var entityInfo = {};
 
 function setRateOfReturn(ror) {
 	rateOfReturn = ror;
-}
-
-function createLink() {
-	$("#form").keydown(function (event) {
-		if(event.keyCode == 13) {
-			console.log(document.getElementById("form").value);
-			alert(document.getElementById("form").value);
-			return document.getElementById("form").value;
-		}
-	})
 }
 
 function addStockToTrackedStocks(ticker) {
@@ -20,6 +12,7 @@ function addStockToTrackedStocks(ticker) {
 		"ticker": ticker
 	};
 	var url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker + ".json?api_key=mSjmVxD7fpFDXBjUsYtT";
+	console.log("the url: " + url);
 	$.ajax({
 		url: url,
 		success: function(data) {
@@ -53,12 +46,80 @@ function recommendFromCurrTrackedStocks() {
 	console.log(recommendedStocks);
 }
 
+function getInput() {
+	var url = "";
+	$("#form").keydown(function (event) {
+			if(event.keyCode == 13) {
+				console.log($("#form").val());
+				url = createLink($("#form").val());
+			}
+		});
+		setTimeout(function() {
+			getJSONresponse(url);
+		}, 5000);
+	}
+
+function createLink(command) {
+  var url = "https://api.projectoxford.ai/luis/v1/application/preview?id=526f6452-0231-4bef-afa5-a8c3c63d470e&subscription-key=baa34ae8054649a49229e587e3eff446&q=";
+  var commandArray = command.split(" ");
+	console.log(commandArray);
+  for (i=0,len=commandArray.length;i<len;i++){
+    url = url + commandArray[i] + "%20";
+  }
+	console.log("url: " + url);
+  return url;
+}
+
+function getJSONresponse(url) {
+	$.getJSON(url, function(data) {
+		JSONresponse.data = data
+		console.log("getJSONResponse:" + data);
+	});
+}
+
+function parseJSON() {
+	entities = JSONresponse.data.entities;
+	console.log("Entities:" + entities);
+	for (i=0;i<entities.length;i++) {
+		switch(entities[i].type) {
+			case "stock_name":
+				entityInfo.stockName = entities[i].entity;
+				console.log("entityInfo " + entityInfo.stockName);
+				break;
+			case "Date::start_date":
+				entityInfo.startDate = entities[i].entity;
+				break;
+			case "Date::end_date":
+				entityInfo.endDate = entities[i].entity;
+				break;
+			case "money":
+				entityInfo.money = entities[i].entity;
+				break;
+			case "want_ROR":
+				entityInfo.wantROR = entities[i].entity;
+				break;
+			case "ROR":
+				entityInfo.actualROR = entities[i].entity;
+				break;
+		}
+	}
+	console.log(JSONresponse.data)
+	// return JSONresponse.data.entities[0].entity; //get name
+}
+
 $(document).ready(function() {
 	setRateOfReturn(30);
-	addStockToTrackedStocks("MSFT");
-	addStockToTrackedStocks("IBM");
-	addStockToTrackedStocks("YHOO");
-	addStockToTrackedStocks("FB");
-	recommendFromCurrTrackedStocks()
-  createLink();
+	getInput();
+	// addStockToTrackedStocks("MSFT");
+	// addStockToTrackedStocks("IBM");
+	// addStockToTrackedStocks("YHOO");
+	// addStockToTrackedStocks("FB");
+	// recommendFromCurrTrackedStocks()
+
+
+	setTimeout(function() {
+			// console.log(parseJSON());
+				addStockToTrackedStocks(entityInfo.stockName);
+	}, 10000	);
+
 });
